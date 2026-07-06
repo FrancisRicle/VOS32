bc3ece7ca4022352e0c1ff98ffd5cf2ef025c3d2
Francis Ricle <francisricle@gmail.com>
2026-07-06 15:18:51 -0300

    syscall handler implemented, re-implemented functions acording to retyping in include/traps.h, timer interrupt hanlder re implemented

--- src/traps/traps.c
+++ src/traps/traps.c
@@ -1,11 +1,34 @@
 #include "traps.h"
+#include "console.h"
+#include "mmu.h"
 #include "panic.h"
 #include "scheduler.h"
-void interrupts_handler(uint32_t pc, uint32_t scause, trap_frame_t *tf) {
+#include <stdint.h>
+void syscall_handler(trap_frame_t *tf) {
+  uint32_t pc = exception_pc();
+  switch (tf->a0) {
+  case 2:
+    putc((char)tf->a1);
+    break;
+  default:
+    panic("SYSCALL UNIMPLEMENTED");
+  }
+  uret(pc + 4);
+}
+void interrupts_handler(uint32_t scause, trap_frame_t *tf) {
+  uint32_t pc = exception_pc();
   switch (CAUSE_ID(scause)) {
   case TIMER_INTERRUPT:
     schedule();
-    yield(pc, tf);
+    uint32_t nextpid = nextproc(currpid);
+    savecurrproc(pc, tf);
+    loadnextproc(nextpid, tf);
+    if (procs[nextpid].epc == PROCS_BASE) {
+      uinterrupts();
+    }
+    rootpt(procs[nextpid].table1);
+    uret(procs[nextpid].epc);
+    currpid = nextpid;
     break;
   case EXTERNAL_INTERRUPT:
     panic("EXTERNAL INTERRUPT HANDLER UNIMPLEMENTED");
@@ -17,26 +40,30 @@ void interrupts_handler(uint32_t pc, uint32_t scause, trap_frame_t *tf) {
     panic("UNHANDLED INTERRUPT");
   }
 }
-void exceptions_handler(uint32_t scause, uint32_t pc, trap_frame_t *tf,
-                        uint32_t stval) {
+void exceptions_handler(uint32_t scause, trap_frame_t *tf) {
   switch (CAUSE_ID(scause)) {
   case USYSCALL_EXP:
+    syscall_handler(tf);
     break;
   case KSYSCALL_EXP:
     break;
   case ILLEGAL_INSTRUPCION_EXP:
     panic("ILLEGAL INSTRUPCION EXCEPTION");
+  case LOAD_PAGE_FAULT_EXP:
+    panic("LOAD PAGE FAULT EXCEPTION");
+  case INSTRUCTION_PAGE_FAULT_EXP:
+    panic("INSTRUCTION PAGE FAULT EXCEPTION");
+  case STORE_PAGE_FAULT_EXP:
+    panic("STORE PAGE FAULT EXCEPTION");
   default:
     panic("UNHANDLED EXCEPTION");
   }
 }
 void traps_handler(trap_frame_t *tf) {
   uint32_t scause = exception_cause();
-  uint32_t stval = exception_value();
-  uint32_t pc = exception_pc();
   if (IS_INTERRUPT(scause)) {
-    interrupts_handler(pc, scause, tf);
+    interrupts_handler(scause, tf);
   } else {
-    exceptions_handler(scause, pc, tf, stval);
+    exceptions_handler(scause, tf);
   }
 }
